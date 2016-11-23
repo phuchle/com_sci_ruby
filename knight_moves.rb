8d7e467579b77e7fa9394d53333e55e4d1b24335
Phuc Le <le.phuc.h@gmail.com>
2016-11-23 16:32:27 -0600

    Finish logic for finding knight moves.

--- knight_moves.rb
+++ knight_moves.rb
@@ -1,26 +1,71 @@
-class Chess
-  attr_accessor :board, :knight
+require 'byebug'
 
-  def initialize
-    @board = [
-      %w(0 1 2 3 4 5 6 7), # 0
-      %w(0 1 2 3 4 5 6 7), # 1
-      %w(0 1 2 3 4 5 6 7), # 2
-      %w(0 1 2 3 4 5 6 7), # 3
-      %w(0 1 2 3 4 5 6 7), # 4
-      %w(0 1 2 3 4 5 6 7), # 5
-      %w(0 1 2 3 4 5 6 7), # 6
-      %w(0 1 2 3 4 5 6 7)  # 7
-    ]
-    @knight = "k"
-    @path = []
-    @move_count = @path.length - 1
-  end
+class Knight
 
   # shows simplest possible way to get from one square to another
   # outputs coordinates of all stops along the way and num of moves taken
   def knight_moves(initial, final)
+    search_queue = [Vertex.new(initial)]
 
+    until search_queue.empty?
+      test_vertex = search_queue.shift
+
+      return show_path(make_path(test_vertex)) if test_vertex.value == final
+      
+      test_vertex.children.each do |value|
+        unless value.any? { |num| num < 0 || num > 7}
+          new_vertex = Vertex.new(value)
+          new_vertex.parent = test_vertex
+          search_queue << new_vertex
+        end
+      end 
+
+    end
+    "No path was found :("
+  end
+  
+  def make_path(vertex)
+    path = []
+    until vertex.nil?
+      path.unshift(vertex.value)
+      vertex = vertex.parent
+    end
+    path
   end
 
+  def show_path(path)
+    if path.length - 1 > 1
+      p "You made it in #{path.length - 1} moves! Here's your path:"
+    else
+      p "You made it in 1 move!  Here's your path:"
+    end
+    path.each { |move| puts "#{move}"}
+  end
 end
+
+class Vertex
+  attr_accessor :value, :parent, :children
+
+  def initialize(value = nil)
+    @value = value
+    @parent = nil
+    @children = [
+      [value[0] + 1, value[1] + 2],
+      [value[0] + 1, value[1] - 2],
+      [value[0] - 1, value[1] + 2],
+      [value[0] - 1, value[1] - 2],
+      [value[0] + 2, value[1] + 1],
+      [value[0] + 2, value[1] - 1],
+      [value[0] - 2, value[1] + 1],
+      [value[0] - 2, value[1] - 1]
+    ]
+
+  end
+end
+
+# Tests
+k = Knight.new
+
+p k.knight_moves([0,0],[1,2]) == [[0,0],[1,2]]
+p k.knight_moves([0,0],[3,3]) == [[0,0],[1,2],[3,3]]
+p k.knight_moves([3,3],[0,0]) == [[3,3],[1,2],[0,0]] || [[3,3],[1,2],[0,0]]
